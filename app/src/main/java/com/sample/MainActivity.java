2a4855d22c59fc6c28c0483fe31ba6ec5832b806
Ahmed Adel Ismail <ahm3d.ad3l@gmail.com>
2018-01-31 04:34:53 +0200

    updated Sample app

--- app/src/main/java/com/sample/MainActivity.java
+++ app/src/main/java/com/sample/MainActivity.java
@@ -16,14 +16,20 @@ public class MainActivity extends AppCompatActivity {
 
         LiteCycle.with(10)
                 .forLifeCycle(this)
-                .onCreateInvoke(i -> Log.e("LiteCycle", "onCreate invoked " + i))
-                .onCreateUpdate(i -> i * 10)
-                .onResumeInvoke(i -> Log.e("LiteCycle", "onResume invoked " + i))
+                .onCreateUpdate(i -> i + 1)
+                .onCreateInvoke(i -> Log.e("LiteCycle", "onCreate() invoked " + i))
+                .onStartUpdate(i -> i + 1)
+                .onStartInvoke(i -> Log.e("LiteCycle", "onStart() invoked " + i))
                 .onResumeUpdate(i -> i + 1)
-                .onPauseInvoke(i -> Log.e("LiteCycle", "onPause invoked " + i))
-                .onDestroyInvoke(i -> Log.e("LiteCycle", "onDestroy() invoked"))
+                .onResumeInvoke(i -> Log.e("LiteCycle", "onResume() invoked " + i))
+                .onPauseUpdate(i -> i + 1)
+                .onPauseInvoke(i -> Log.e("LiteCycle", "onPause() invoked " + i))
+                .onStopUpdate(i -> i + 1)
+                .onStartInvoke(i -> Log.e("LiteCycle", "onStop() invoked " + i))
                 .onDestroyUpdate(i -> 10)
-                .onFinishingInvoke(i -> Log.e("LiteCycle", "isFinishing() invoked"))
+                .onDestroyInvoke(i -> Log.e("LiteCycle", "onDestroy() invoked " + i))
+                .onFinishingUpdate(i -> null)
+                .onFinishingInvoke(i -> Log.e("LiteCycle", "isFinishing() invoked " + i))
                 .observe();
 
 
